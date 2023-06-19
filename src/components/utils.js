28b6a3acaa28ca86836684c0c969a5f347e33cec
CrisGraciano <cristobalgraciano98@gmail.com>
2023-06-18 22:29:19 -0600

    utils 2 functions done

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,3 +1,19 @@
-// fetch all estimates from json file
+import estimates from '../JSON/getAllEstimatesResponse.json';
 
+// fetch all estimates from json file
+export async function fetchEstimates() {
+  return estimates;
+}
 // filter estimated by selected chip
+export async function searchEstimateByFilter(filter) {
+  if (filter === 'all') {
+    return estimates;
+  }
+  const filteredEstimates = estimates.filter((estimates) => {
+    if (filter === status_label) {
+      return estimates;
+    }
+  });
+
+  return filteredEstimates;
+}
