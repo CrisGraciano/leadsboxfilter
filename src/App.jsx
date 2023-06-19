3ebebfd00d5e12fbde05899472957577bdaac1e6
CrisGraciano <cristobalgraciano98@gmail.com>
2023-06-18 22:45:29 -0600

    app.js file cleaned

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,21 @@
-/* eslint-disable no-unreachable */
-import React, { useState } from 'react';
-import './App.scss';
-import DbResponses from './JSON/getAllEstimatesResponse.json';
-import FilterChips from './components/UI/FilterChips';
+import React, { useState, useEffect } from 'react';
+import { fetchEstimates, searchEstimateByFilter } from './components/utils';
 
+// 1. Respuesta que obtenemos del servidor en formato JSON.
+// Podes verla en la consola del navegador.
+// Procesa los datos para que puedas usarlos en el componente de la forma que quieras.
+// Los datos que te serviran son dentro de las quotes: status_label, status_class, quote_status_id
+// Y ME OLVIDE DE MOSTRATE: "quote_statuses" es un array de objetos que tiene los datos base los status.
+// eslint-disable-next-line no-console
+// 2. Este es el array de objetos que se esta usando para mostrar los filtros. Te recomiendo crear uno nuevo a partir de los datos sin procesar con los datos extras necesarios cumplir los requirimientos.
+// Tene en cuenta al procesar los datos que te da el servidor, que el valor de "value" tiene que ser unico (por eso no solo usamos Active, etc).
+// El valor de "label" es el que se muestra en pantalla.
+// Extra: el valor no necesariamente es una sola pablabra, puede ser un string con espacios, como "partially paid".
 function App() {
-  // 1. Respuesta que obtenemos del servidor en formato JSON.
-  // Podes verla en la consola del navegador.
-  // Procesa los datos para que puedas usarlos en el componente de la forma que quieras.
-  // Los datos que te serviran son dentro de las quotes: status_label, status_class, quote_status_id
-  // Y ME OLVIDE DE MOSTRATE: "quote_statuses" es un array de objetos que tiene los datos base los status.
-  const data = DbResponses;
-  // eslint-disable-next-line no-console
-  console.log(data);
-
-  // 2. Este es el array de objetos que se esta usando para mostrar los filtros. Te recomiendo crear uno nuevo a partir de los datos sin procesar con los datos extras necesarios cumplir los requirimientos.
-  // Tene en cuenta al procesar los datos que te da el servidor, que el valor de "value" tiene que ser unico (por eso no solo usamos Active, etc).
-  // El valor de "label" es el que se muestra en pantalla.
-  // Extra: el valor no necesariamente es una sola pablabra, puede ser un string con espacios, como "partially paid".
   return (
     <div className="App m-5">
       <h1>Hello LeadsBox!</h1>
       <h2 className="mb-5">Filter chips component</h2>
-
-      <p>Requisitos:</p>
-      <ol>
-        <li>Cada chip debe tener un color que lo distinga de los demas</li>
-        <li>Debe poder seleccionarse un reset de estado (all) </li>
-        <li>
-          Se debe mostar la cantidad de elementos existentes de cada tipo de
-          filtro. ej: Active (2)
-        </li>
-        <li>Se debe indicar cual chip ha sido seleccionada</li>
-        <li>
-          Como extra podes probar usar prop-types si borras la linea 2 del
-          componente.
-        </li>
-        <li>Testear en mobile y desktop</li>
-      </ol>
     </div>
   );
 }
